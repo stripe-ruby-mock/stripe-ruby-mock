73c6a8d9d110354dd0ce8e6b3395a221e183a0bd
Gilbert <gilbertbgarza@gmail.com>
2013-05-19 14:40:05 -0500

    Refactored api into its own folder

--- lib/stripe_mock.rb
+++ lib/stripe_mock.rb
@@ -3,35 +3,9 @@ require 'ostruct'
 require 'stripe_mock/version'
 require 'stripe_mock/data'
 
+require 'stripe_mock/api/instance'
+
 require 'stripe_mock/request_handlers/charges.rb'
 require 'stripe_mock/request_handlers/customers.rb'
 require 'stripe_mock/request_handlers/invoice_items.rb'
 require 'stripe_mock/instance'
-
-module StripeMock
-
-  @@first_start = true
-  @@instance = nil
-
-  def self.start
-    if @@first_start == true
-      @@original_request_method = Stripe.method(:request)
-      @@first_start = false
-    end
-    @@instance = Instance.new
-    alias_stripe_method :request, @@instance.method(:mock_request)
-  end
-
-  def self.stop
-    return if @@instance.nil?
-    alias_stripe_method :request, @@original_request_method
-    @@instance = nil
-  end
-
-  def self.alias_stripe_method(new_name, method_object)
-    Stripe.define_singleton_method(new_name) {|*args| method_object.call(*args) }
-  end
-
-  def self.instance; @@instance; end
-
-end
