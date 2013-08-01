31e8c9c1abb0b9b394c8d83739f275fdb34296d1
Gilbert <gilbertbgarza@gmail.com>
2013-08-01 14:13:09 -0500

    Wrote breaking test for #17

--- spec/shared_stripe_examples/card_token_examples.rb
+++ spec/shared_stripe_examples/card_token_examples.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 shared_examples 'Card Token Mocking' do
 
-  it "generates a card token" do
+  it "generates and reads a card token for create customer" do
     card_token = StripeMock.generate_card_token(last4: "9191", exp_month: 99, exp_year: 3005)
 
     cus = Stripe::Customer.create(card: card_token)
@@ -10,6 +10,19 @@ shared_examples 'Card Token Mocking' do
     expect(card.last4).to eq("9191")
     expect(card.exp_month).to eq(99)
     expect(card.exp_year).to eq(3005)
+  end
+
+  it "generated and reads a card token for update customer" do
+    card_token = StripeMock.generate_card_token(last4: "1133", exp_month: 11, exp_year: 2099)
+
+    cus = Stripe::Customer.create()
+    cus.card = card_token
+    cus.save
+
+    card = cus.cards.data.first
+    expect(card.last4).to eq("1133")
+    expect(card.exp_month).to eq(11)
+    expect(card.exp_year).to eq(2099)
   end
 
 end
