eabba34bab0baaead5809962c18a3ec60f036730
Joshua Krall <joshuakrall@pobox.com>
2013-08-31 23:06:41 -0700

    Fixing error messages to match live Stripe

--- lib/stripe_mock/api/errors.rb
+++ lib/stripe_mock/api/errors.rb
@@ -24,16 +24,16 @@ module StripeMock
 
     def self.argument_map
       @__map ||= {
-        incorrect_number: ["The card number is incorrect", 'number', 'incorrect_number', 402],
-        invalid_number: ["The card number is not a valid credit card number", 'number', 'invalid_number', 402],
-        invalid_expiry_month: ["The card's expiration month is invalid", 'exp_month', 'invalid_expiry_month', 402],
-        invalid_expiry_year: ["The card's expiration year is invalid", 'exp_year', 'invalid_expiry_year', 402],
-        invalid_cvc: ["The card's security code is invalid", 'cvc', 'invalid_cvc', 402],
-        expired_card: ["The card has expired", 'exp_month', 'expired_card', 402],
-        incorrect_cvc: ["The card's security code is incorrect", 'cvc', 'incorrect_cvc', 402],
-        card_declined: ["The card was declined", nil, 'card_declined', 402],
+        incorrect_number: ["Your card number is incorrect.", 'number', 'incorrect_number', 402],
+        invalid_number: ["Your card number is not a valid credit card number.", 'number', 'invalid_number', 402],
+        invalid_expiry_month: ["Your card's expiration month is invalid.", 'exp_month', 'invalid_expiry_month', 402],
+        invalid_expiry_year: ["Your card's expiration year is invalid.", 'exp_year', 'invalid_expiry_year', 402],
+        invalid_cvc: ["Your card's security code is invalid", 'cvc', 'invalid_cvc', 402],
+        expired_card: ["Your card has expired.", 'exp_month', 'expired_card', 402],
+        incorrect_cvc: ["Your card's security code is incorrect.", 'cvc', 'incorrect_cvc', 402],
+        card_declined: ["Your card was declined.", nil, 'card_declined', 402],
         missing: ["There is no card on a customer that is being charged.", nil, 'missing', 402],
-        processing_error: ["An error occurred while processing the card", nil, 'processing_error', 402],
+        processing_error: ["An error occurred while processing your card.", nil, 'processing_error', 402],
       }
     end
   end
