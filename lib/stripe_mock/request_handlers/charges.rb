3d0d11cc56604dfb77b0712fdbfb3499e00c066a
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-05 17:37:00 -0700

    finishing support for recipient, both new and get

--- lib/stripe_mock/request_handlers/charges.rb
+++ lib/stripe_mock/request_handlers/charges.rb
@@ -6,6 +6,8 @@ module StripeMock
         klass.add_handler 'post /v1/charges',               :new_charge
         klass.add_handler 'get /v1/charges/(.*)',           :get_charge
         klass.add_handler 'post /v1/charges/(.*)/capture',  :capture_charge
+        klass.add_handler 'post /v1/recipients',            :new_recipient
+        klass.add_handler 'get /v1/recipients/(.*)',        :get_recipient
       end
 
       def new_charge(route, method_url, params, headers)
@@ -28,6 +30,16 @@ module StripeMock
         charge
       end
 
+      def new_recipient(route, method_url, params, headers)
+        id = new_id('rp')
+        recipients[id] = Data.mock_recipient(params.merge :id => id)
+      end
+
+      def get_recipient(route, method_url, params, headers)
+        route =~ method_url
+        assert_existance :recipient, $1, recipients[$1]
+        recipients[$1] ||= Data.mock_recipient(:id => $1)
+      end
     end
   end
 end
