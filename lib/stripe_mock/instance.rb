e6e1a905c2a6158403da502ad1cad3157f1292df
Dylan Jhaveri <dylanjhaveri@gmail.com>
2013-09-23 18:43:08 -0700

    no need to have cards attr_reader, cars are always stored inside of a customer

--- lib/stripe_mock/instance.rb
+++ lib/stripe_mock/instance.rb
@@ -22,13 +22,12 @@ module StripeMock
     include StripeMock::RequestHandlers::Plans
 
 
-    attr_reader :charges, :customers, :plans, :error_queue, :recipients, :cards
+    attr_reader :charges, :customers, :plans, :error_queue, :recipients
     attr_accessor :debug, :strict
 
     def initialize
       @customers = {}
       @recipients = {}
-      @cards = {}
       @charges = {}
       @plans = {}
       @recipient_tokens = {}
