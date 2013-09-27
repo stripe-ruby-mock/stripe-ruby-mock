7a66ffae67b8ab843e66550ec06e00f0f6a46db7
Gilbert <gilbertbgarza@gmail.com>
2013-09-27 09:55:13 -0500

    Formatting

--- spec/shared_stripe_examples/recipient_examples.rb
+++ spec/shared_stripe_examples/recipient_examples.rb
@@ -23,15 +23,15 @@ shared_examples 'Recipient API' do
 
   it "stores a created stripe recipient in memory" do
     recipient = Stripe::Recipient.create({
-                                      type:  "individual",
-                                      name: "Customer One",
-                                      bank_account: 'bank_account_token_1'
-                                    })
+      type:  "individual",
+      name: "Customer One",
+      bank_account: 'bank_account_token_1'
+    })
     recipient2 = Stripe::Recipient.create({
-                                      type:  "individual",
-                                      name: "Customer Two",
-                                      bank_account: 'bank_account_token_1'
-                                    })
+      type:  "individual",
+      name: "Customer Two",
+      bank_account: 'bank_account_token_1'
+    })
     data = test_data_source(:recipients)
     expect(data[recipient.id]).to_not be_nil
     expect(data[recipient.id][:name]).to eq("Customer One")
