1fd490818e2ad575f45ab9a863e8d749521a63d9
Greg Yardley <greg@yardley.ca>
2014-02-13 14:53:37 -0300

    Support for multiple subscriptions

--- spec/support/stripe_examples.rb
+++ spec/support/stripe_examples.rb
@@ -10,6 +10,7 @@ def require_stripe_examples
   require 'shared_stripe_examples/invoice_item_examples'
   require 'shared_stripe_examples/plan_examples'
   require 'shared_stripe_examples/recipient_examples'
+  require 'shared_stripe_examples/subscription_examples'
   require 'shared_stripe_examples/webhook_event_examples'
 end
 
@@ -24,5 +25,6 @@ def it_behaves_like_stripe(&block)
   it_behaves_like 'Plan API', &block
   it_behaves_like 'Recipient API', &block
   it_behaves_like 'Stripe Error Mocking', &block
+  it_behaves_like 'Customer Subscriptions', &block
   it_behaves_like 'Webhook Events API', &block
 end
