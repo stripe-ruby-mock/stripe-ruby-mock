901d4930d26138079afd9030856ed66bb879186b
Nathan Woodhull <woodhull@gmail.com>
2014-08-05 16:51:32 -0400

    remove failure.

--- spec/shared_stripe_examples/account_examples.rb
+++ spec/shared_stripe_examples/account_examples.rb
@@ -5,15 +5,4 @@ shared_examples 'Account API' do
     account = Stripe::Account.retrieve
     expect(account).to respond_to(:display_name)
   end
-
-  context "With strict mode toggled off" do
-
-    before { StripeMock.toggle_strict(false) }
-
-    it "retrieves a stripe customer with an id that doesn't exist" do
-      account = Stripe::Account.retrieve('test_account_x')
-      expect(account.id).to eq('test_account_x')
-      expect(account.display_name).to_not be_nil
-    end
-  end
 end
