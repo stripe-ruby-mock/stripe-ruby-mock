d0436d73d285845cf32518c546171fe0a6ca7144
Gilbert <gilbertbgarza@gmail.com>
2014-08-09 16:07:57 -0500

    Add guard against missing env var for live tests; update var name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,15 @@ RSpec.configure do |c|
     StripeMock.set_default_test_helper_strategy(:live)
     c.filter_run_excluding :mock_server => true
 
+    api_key = ENV['STRIPE_TEST_SECRET_KEY']
+    if api_key.nil? || api_key == ''
+      raise "Please set your STRIPE_TEST_SECRET_KEY environment variable."
+    end
+
     c.before(:each) do
       StripeMock.stub(:start).and_return(nil)
       StripeMock.stub(:stop).and_return(nil)
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+      Stripe.api_key = api_key
     end
     c.after(:each) { sleep 1 }
   end
