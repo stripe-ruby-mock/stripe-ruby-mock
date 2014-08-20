ea87a7833ae4258576d6a5a52e039aebc7d0c60f
Nikolai Berkoff <alex.frost.dev@gmail.com>
2014-08-20 22:37:43 +0100

    refactor and dry up

--- lib/stripe_mock/api/server.rb
+++ lib/stripe_mock/api/server.rb
@@ -1,24 +1,17 @@
 module StripeMock
 
-  @default_pid_path = './stripe-mock-server.pid'
-  @default_log_path = './stripe-mock-server.log'
-
   class << self
+    ["pid", "log"].each do |config_type|
+      attr_writer "default_server_#{config_type}_path".to_sym
 
-    def default_server_pid_path; @default_pid_path; end
-    def default_server_pid_path=(new_path)
-      @default_pid_path = new_path
+      define_method("default_server_#{config_type}_path") do
+        instance_variable_get("@default_server_#{config_type}_path") || "./stripe-mock-server.#{config_type}"
+      end
     end
-
-    def default_server_log_path; @default_log_path; end
-    def default_server_log_path=(new_path)
-      @default_log_path = new_path
-    end
-
 
     def spawn_server(opts={})
-      pid_path = opts[:pid_path] || @default_pid_path
-      log_path = opts[:log_path] || @default_log_path
+      pid_path = opts[:pid_path] || default_server_pid_path
+      log_path = opts[:log_path] || default_server_log_path
 
       Dante::Runner.new('stripe-mock-server').execute(
         :daemonize => true, :pid_path => pid_path, :log_path => log_path
@@ -30,9 +23,8 @@ module StripeMock
 
     def kill_server(pid_path=nil)
       puts "Killing server at #{pid_path}"
-      path = pid_path || @default_pid_path
+      path = pid_path || default_server_pid_path
       Dante::Runner.new('stripe-mock-server').execute(:kill => true, :pid_path => path)
     end
   end
-
 end
