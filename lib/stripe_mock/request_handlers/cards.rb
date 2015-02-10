45ad543bceff9591c0fefc890d2400b49788653a
Gilbert <gilbertbgarza@gmail.com>
2015-02-10 10:32:54 -0600

    Move card helpers to card helpers file

--- lib/stripe_mock/request_handlers/cards.rb
+++ lib/stripe_mock/request_handlers/cards.rb
@@ -64,42 +64,6 @@ module StripeMock
         card
       end
 
-      private
-
-      def delete_card_from(type, type_id, card_id, objects)
-        resource = assert_existence type, type_id, objects[type_id]
-
-        assert_existence :card, card_id, get_card(resource, card_id)
-
-        card = { id: card_id, deleted: true }
-        resource[:cards][:data].reject!{|cc|
-          cc[:id] == card[:id]
-        }
-        resource[:default_card] = resource[:cards][:data].count > 0 ? resource[:cards][:data].first[:id] : nil
-        card
-      end
-
-      def add_card_to(type, type_id, params, objects)
-        resource = assert_existence type, type_id, objects[type_id]
-
-        card = card_from_params(params[:card])
-        add_card_to_object(type, card, resource)
-      end
-
-      def validate_card(card)
-        [:exp_month, :exp_year].each do |field|
-          card[field] = card[field].to_i
-        end
-        card
-      end
-
-      def card_from_params(attrs_or_token)
-        if attrs_or_token.is_a? Hash
-          attrs_or_token = generate_card_token(attrs_or_token)
-        end
-        card = get_card_by_token(attrs_or_token)
-        validate_card(card)
-      end
     end
   end
 end
