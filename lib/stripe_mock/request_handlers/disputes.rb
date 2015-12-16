b414e504f569004db406be143c5a8d14c379506e
Noman Ur Rehman <nomanurrehman@gmail.com>
2015-12-17 00:20:02 +0500

    Add request handlers for disputes

--- lib/stripe_mock/request_handlers/disputes.rb
+++ lib/stripe_mock/request_handlers/disputes.rb
@@ -5,11 +5,14 @@ module StripeMock
       def Disputes.included(klass)
         klass.add_handler 'get /v1/disputes/(.*)',        :get_dispute
         klass.add_handler 'post /v1/disputes/(.*)',       :update_dispute
-        klass.add_handler 'post /v1/disputes/(.*)/close'  :close_dispute
-        klass.add_handler 'get /v1/disputes',             :get_all_disputes    
+        klass.add_handler 'post /v1/disputes/(.*)/close', :close_dispute
+        klass.add_handler 'get /v1/disputes',             :list_disputes    
       end
 
       def get_dispute(route, method_url, params, headers)
+        route =~ method_url
+        disputes[$1] = Data.mock_dispute(id: $1)
+        assert_existence :dispute, $1, disputes[$1]
       end
 
       def update_dispute(route, method_url, params, headers)
@@ -18,7 +21,8 @@ module StripeMock
       def close_dispute(route, method_url, params, headers)
       end
 
-      def get_all_disputes(route, method_url, params, headers)
+      def list_disputes(route, method_url, params, headers)
+        Data.mock_list_object(disputes.values, params)
       end
 
     end
