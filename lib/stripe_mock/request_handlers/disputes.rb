5fc8ff9f2af4b48f39a6627fad1d68bfe943c666
Noman Ur Rehman <nomanurrehman@gmail.com>
2015-12-19 21:41:43 +0500

    Clean up dispute endpoints implementation

--- lib/stripe_mock/request_handlers/disputes.rb
+++ lib/stripe_mock/request_handlers/disputes.rb
@@ -6,26 +6,23 @@ module StripeMock
         klass.add_handler 'get /v1/disputes/(.*)',        :get_dispute
         klass.add_handler 'post /v1/disputes/(.*)',       :update_dispute
         klass.add_handler 'post /v1/disputes/(.*)/close', :close_dispute
-        klass.add_handler 'get /v1/disputes',             :list_disputes    
+        klass.add_handler 'get /v1/disputes',             :list_disputes   
       end
 
       def get_dispute(route, method_url, params, headers)
         route =~ method_url
-        disputes[$1] = Data.mock_dispute(id: $1)
         assert_existence :dispute, $1, disputes[$1]
       end
 
       def update_dispute(route, method_url, params, headers)
-        route =~ method_url
-        dispute = assert_existence :dispute, $1, disputes[$1]
+        dispute = get_dispute(route, method_url, params, headers)
         dispute.merge!(params)
         dispute
       end
 
       def close_dispute(route, method_url, params, headers)
-        route =~ method_url
-        dispute = assert_existence :dispute, $1, disputes[$1]
-        dispute[:status] = "lost"
+        dispute = get_dispute(route, method_url, params, headers)
+        dispute.merge!({:status => 'lost'})
         dispute
       end
 
