e884a58ca47ab2f7eb6c4f63732d16566b97c65e
Noman Ur Rehman <nomanurrehman@gmail.com>
2015-12-28 00:00:24 +0500

    Add api endpoint to retrieve all events

--- lib/stripe_mock/request_handlers/events.rb
+++ lib/stripe_mock/request_handlers/events.rb
@@ -4,6 +4,7 @@ module StripeMock
 
       def Events.included(klass)
         klass.add_handler 'get /v1/events/(.*)', :retrieve_event
+        klass.add_handler 'get /v1/events',      :list_events 
       end
 
       def retrieve_event(route, method_url, params, headers)
@@ -11,6 +12,10 @@ module StripeMock
         assert_existence :event, $1, events[$1]
       end
 
+      def list_events(route, method_url, params, headers)
+        Data.mock_list_object(events.values, params)
+      end
+      
     end
   end
 end
