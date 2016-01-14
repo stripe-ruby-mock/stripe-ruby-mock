c55cdfa019c9ce1346ba3f9db850f5a1c676cdf4
Geoffrey Thompson <fleaplus@gmail.com>
2016-01-14 02:12:58 -0700

    Allow and preserve arguments when spawning a StripeMock server.

--- lib/stripe_mock/api/server.rb
+++ lib/stripe_mock/api/server.rb
@@ -16,18 +16,33 @@ module StripeMock
       pid_path = opts[:pid_path] || default_server_pid_path
       log_path = opts[:log_path] || default_server_log_path
 
+      argv_backup = ARGV.dup
+      ARGV.clear
+
       Dante::Runner.new('stripe-mock-server').execute(
         :daemonize => true, :pid_path => pid_path, :log_path => log_path
       ){
         StripeMock::Server.start_new(opts)
       }
+      argv_backup.each do |arg|
+        ARGV << arg
+      end
+
       at_exit { kill_server(pid_path) }
     end
 
     def kill_server(pid_path=nil)
       puts "Killing server at #{pid_path}"
       path = pid_path || default_server_pid_path
+      
+      argv_backup = ARGV.dup
+      ARGV.clear
+
       Dante::Runner.new('stripe-mock-server').execute(:kill => true, :pid_path => path)
+      
+      argv_backup.each do |arg|
+        ARGV << arg
+      end
     end
   end
 end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -9,127 +9,139 @@ describe 'StripeMock Server', :mock_server => true do
     def test_data_source(type); StripeMock.client.get_server_data(type); end
   end
 
-  before(:all) do
-    StripeMock.spawn_server
-  end
+  context "without arguments" do
+    before(:all) do
+      StripeMock.spawn_server
+    end
 
-  before do
-    @client = StripeMock.start_client
-  end
+    before do
+      @client = StripeMock.start_client
+    end
 
-  after { StripeMock.stop_client(:clear_server_data => true) }
-
-
-  it "uses an RPC client for mock requests" do
-    charge = Stripe::Charge.create(
-      amount: 987,
-      currency: 'USD',
-      card: stripe_helper.generate_card_token,
-      description: 'card charge'
-    )
-    expect(charge.amount).to eq(987)
-    expect(charge.currency).to eq('USD')
-    expect(charge.description).to eq('card charge')
-  end
+    after { StripeMock.stop_client(:clear_server_data => true) }
 
 
-  it "should not clear server data in between client sessions by default" do
-    customer = Stripe::Customer.create(email: 'johnny@appleseed.com')
-    expect(customer.email).to eq('johnny@appleseed.com')
-
-    server_customer_data = StripeMock.client.get_server_data(:customers)[customer.id]
-    expect(server_customer_data).to_not be_nil
-    expect(server_customer_data[:email]).to eq('johnny@appleseed.com')
-
-    StripeMock.stop_client
-    StripeMock.start_client
-
-    server_customer_data = StripeMock.client.get_server_data(:customers)[customer.id]
-    expect(server_customer_data).to_not be_nil
-    expect(server_customer_data[:email]).to eq('johnny@appleseed.com')
-  end
+    it "uses an RPC client for mock requests" do
+      charge = Stripe::Charge.create(
+        amount: 987,
+        currency: 'USD',
+        card: stripe_helper.generate_card_token,
+        description: 'card charge'
+      )
+      expect(charge.amount).to eq(987)
+      expect(charge.currency).to eq('USD')
+      expect(charge.description).to eq('card charge')
+    end
 
 
-  it "returns a response with symbolized hash keys" do
-    stripe_helper.create_plan(id: 'x')
-    response, api_key = StripeMock.redirect_to_mock_server('get', '/v1/plans/x', 'xxx')
-    response.keys.each {|k| expect(k).to be_a(Symbol) }
-  end
+    it "should not clear server data in between client sessions by default" do
+      customer = Stripe::Customer.create(email: 'johnny@appleseed.com')
+      expect(customer.email).to eq('johnny@appleseed.com')
+
+      server_customer_data = StripeMock.client.get_server_data(:customers)[customer.id]
+      expect(server_customer_data).to_not be_nil
+      expect(server_customer_data[:email]).to eq('johnny@appleseed.com')
+
+      StripeMock.stop_client
+      StripeMock.start_client
+
+      server_customer_data = StripeMock.client.get_server_data(:customers)[customer.id]
+      expect(server_customer_data).to_not be_nil
+      expect(server_customer_data[:email]).to eq('johnny@appleseed.com')
+    end
 
 
-  it "can toggle debug" do
-    StripeMock.toggle_debug(true)
-    expect(@client.server_debug?).to eq(true)
-    StripeMock.toggle_debug(false)
-    expect(@client.server_debug?).to eq(false)
-  end
+    it "returns a response with symbolized hash keys" do
+      stripe_helper.create_plan(id: 'x')
+      response, api_key = StripeMock.redirect_to_mock_server('get', '/v1/plans/x', 'xxx')
+      response.keys.each {|k| expect(k).to be_a(Symbol) }
+    end
 
 
-  it "should toggle off debug when mock session ends" do
-    StripeMock.toggle_debug(true)
-
-    StripeMock.stop_client
-    expect(StripeMock.client).to be_nil
-
-    StripeMock.start_client
-    expect(StripeMock.client.server_debug?).to eq(false)
-  end
+    it "can toggle debug" do
+      StripeMock.toggle_debug(true)
+      expect(@client.server_debug?).to eq(true)
+      StripeMock.toggle_debug(false)
+      expect(@client.server_debug?).to eq(false)
+    end
 
 
-  it "can set the default server pid path" do
-    expect(StripeMock.default_server_pid_path).to eq('./stripe-mock-server.pid')
+    it "should toggle off debug when mock session ends" do
+      StripeMock.toggle_debug(true)
 
-    orig = StripeMock.default_server_pid_path
-    StripeMock.default_server_pid_path = 'abc'
-    expect(StripeMock.default_server_pid_path).to eq('abc')
+      StripeMock.stop_client
+      expect(StripeMock.client).to be_nil
 
-    # Set back to original for #kill_server to work properly
-    StripeMock.default_server_pid_path = orig
-  end
-
-  it "can set the default server log path" do
-    expect(StripeMock.default_server_log_path).to eq('./stripe-mock-server.log')
-
-    StripeMock.default_server_log_path = 'yule.log'
-    expect(StripeMock.default_server_log_path).to eq('yule.log')
-  end
+      StripeMock.start_client
+      expect(StripeMock.client.server_debug?).to eq(false)
+    end
 
 
-  it "doesn't create multiple clients" do
-    result = StripeMock.start_client
-    expect(result.__id__).to eq(@client.__id__)
-  end
+    it "can set the default server pid path" do
+      expect(StripeMock.default_server_pid_path).to eq('./stripe-mock-server.pid')
+
+      orig = StripeMock.default_server_pid_path
+      StripeMock.default_server_pid_path = 'abc'
+      expect(StripeMock.default_server_pid_path).to eq('abc')
+
+      # Set back to original for #kill_server to work properly
+      StripeMock.default_server_pid_path = orig
+    end
+
+    it "can set the default server log path" do
+      expect(StripeMock.default_server_log_path).to eq('./stripe-mock-server.log')
+
+      StripeMock.default_server_log_path = 'yule.log'
+      expect(StripeMock.default_server_log_path).to eq('yule.log')
+    end
 
 
-  it "raises an error when client is stopped" do
-    expect(@client).to be_a StripeMock::Client
-    expect(@client.state).to eq('ready')
-
-    StripeMock.stop_client
-    expect(@client.state).to eq('closed')
-    expect { @client.clear_server_data }.to raise_error StripeMock::ClosedClientConnectionError
-  end
+    it "doesn't create multiple clients" do
+      result = StripeMock.start_client
+      expect(result.__id__).to eq(@client.__id__)
+    end
 
 
-  it "raises an error when client connection is closed" do
-    expect(@client).to be_a StripeMock::Client
-    expect(@client.state).to eq('ready')
+    it "raises an error when client is stopped" do
+      expect(@client).to be_a StripeMock::Client
+      expect(@client.state).to eq('ready')
 
-    @client.close!
-    expect(@client.state).to eq('closed')
-    expect(StripeMock.stop_client).to eq(false)
-  end
+      StripeMock.stop_client
+      expect(@client.state).to eq('closed')
+      expect { @client.clear_server_data }.to raise_error StripeMock::ClosedClientConnectionError
+    end
 
 
-  it "throws an error when server is not running" do
-    StripeMock.stop_client
-    begin
-      StripeMock.start_client(1515)
-      # We should never get here
-      expect(false).to eq(true)
-    rescue StripeMock::ServerTimeoutError => e
-      expect(e.associated_error).to be_a(Errno::ECONNREFUSED)
+    it "raises an error when client connection is closed" do
+      expect(@client).to be_a StripeMock::Client
+      expect(@client.state).to eq('ready')
+
+      @client.close!
+      expect(@client.state).to eq('closed')
+      expect(StripeMock.stop_client).to eq(false)
+    end
+
+
+    it "throws an error when server is not running" do
+      StripeMock.stop_client
+      begin
+        StripeMock.start_client(1515)
+        # We should never get here
+        expect(false).to eq(true)
+      rescue StripeMock::ServerTimeoutError => e
+        expect(e.associated_error).to be_a(Errno::ECONNREFUSED)
+      end
     end
   end
 
+  context "with arguments" do
+    it "preserves passed arguments" do
+      ARGV << "--test"
+
+      StripeMock.spawn_server
+
+      expect(ARGV).to include "--test"
+      ARGV.delete "--test"
+    end
+  end
 end
