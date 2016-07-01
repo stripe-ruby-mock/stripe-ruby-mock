6f689278845acd7922a56605fe0dd745e72ef919
Rob Cameron <cannikinn@gmail.com>
2016-07-01 12:13:23 -0700

    Include subscription object when checking whether a card is required or not

--- lib/stripe_mock/request_handlers/subscriptions.rb
+++ lib/stripe_mock/request_handlers/subscriptions.rb
@@ -44,11 +44,11 @@ module StripeMock
           customer[:default_source] = new_card[:id]
         end
 
-        # Ensure customer has card to charge if plan has no trial and is not free
-        verify_card_present(customer, plan, params)
-
         subscription = Data.mock_subscription({ id: (params[:id] || new_id('su')) })
         subscription.merge!(custom_subscription_params(plan, customer, params))
+
+        # Ensure customer has card to charge if plan has no trial and is not free
+        verify_card_present(customer, plan, subscription, params)
 
         if params[:coupon]
           coupon_id = params[:coupon]
@@ -84,11 +84,11 @@ module StripeMock
           customer[:default_source] = new_card[:id]
         end
 
-        # Ensure customer has card to charge if plan has no trial and is not free
-        verify_card_present(customer, plan, params)
-
         subscription = Data.mock_subscription({ id: (params[:id] || new_id('su')) })
         subscription.merge!(custom_subscription_params(plan, customer, params))
+
+        # Ensure customer has card to charge if plan has no trial and is not free
+        verify_card_present(customer, plan, subscription, params)
 
         if params[:coupon]
           coupon_id = params[:coupon]
@@ -155,7 +155,7 @@ module StripeMock
 
         assert_existence :plan, plan_name, plan
         params[:plan] = plan if params[:plan]
-        verify_card_present(customer, plan)
+        verify_card_present(customer, plan, subscription)
 
         if subscription[:cancel_at_period_end]
           subscription[:cancel_at_period_end] = false
@@ -200,9 +200,8 @@ module StripeMock
 
       private
 
-      def verify_card_present(customer, plan, params={})
-        if customer[:default_source].nil? && customer[:trial_end].nil? && plan[:trial_period_days].nil? &&
-           plan[:amount] != 0 && plan[:trial_end].nil? && params[:trial_end].nil?
+      def verify_card_present(customer, plan, subscription, params={})
+        if customer[:default_source].nil? && customer[:trial_end].nil? && plan[:trial_period_days].nil? && plan[:amount] != 0 && plan[:trial_end].nil? && params[:trial_end].nil? && (subscription.nil? || subscription[:trial_end].nil?)
           raise Stripe::InvalidRequestError.new('You must supply a valid card xoxo', nil, 400)
         end
       end
