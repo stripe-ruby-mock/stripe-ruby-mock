2ebb55a60da508b0a56aeb0a28e83b841ef1967b
Bart Stevens <bstevens@solutionstreet.com>
2016-09-30 12:34:23 -0400

    Initial commit

--- lib/stripe_mock/instance.rb
+++ lib/stripe_mock/instance.rb
@@ -116,7 +116,10 @@ module StripeMock
 
     def renew_subscription(subscription_id)
       if @subscriptions.has_key?(subscription_id)
-        @subscriptions[subscription_id].renew_subscription
+        subscription = assert_existence :subscription, subscription_id, @subscriptions[subscription_id]
+      end
+      if subscription.present?
+        subscription.renew_subscription
       else
         raise "Unable to renew subscription #{subscription_id} because this subscription does not exist"
       end
--- lib/stripe_mock/test_strategies/mock.rb
+++ lib/stripe_mock/test_strategies/mock.rb
@@ -19,12 +19,13 @@ puts "StripeMock.state=#{StripeMock.state}"
         if StripeMock.state == 'remote'
           StripeMock.client.renew_subscription(subscription_id)
         elsif StripeMock.state == 'local'
-          subscription = assert_existence :subscription, subscription_id, StripeMock.instance.subscriptions[subscription_id]
-          if subscription.present?
-            subscription.renew_subscription
-          else
-            raise "Unable to renew subscription #{subscription_id} because this subscription does not exist"
-          end
+          StripeMock.instance.renew_subscription(subscription_id)
+#          subscription = assert_existence :subscription, subscription_id, StripeMock.instance.subscriptions[subscription_id]
+#          if subscription.present?
+#            subscription.renew_subscription
+#          else
+#            raise "Unable to renew subscription #{subscription_id} because this subscription does not exist"
+#          end
         end
       end
 
