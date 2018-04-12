10b83a75e837f1f3e411720a9948cb5d9346940a
Alex Mamonchik <alex.mamonchik@gmail.com>
2018-04-12 21:04:49 +0600

    fixed mistakes in merge

--- lib/stripe_mock.rb
+++ lib/stripe_mock.rb
@@ -49,6 +49,7 @@ require 'stripe_mock/request_handlers/validators/param_validators.rb'
 
 require 'stripe_mock/request_handlers/accounts.rb'
 require 'stripe_mock/request_handlers/external_accounts.rb'
+require 'stripe_mock/request_handlers/balance.rb'
 require 'stripe_mock/request_handlers/balance_transactions.rb'
 require 'stripe_mock/request_handlers/charges.rb'
 require 'stripe_mock/request_handlers/cards.rb'
