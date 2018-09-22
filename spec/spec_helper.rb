d76139011f988e6cb707843cba737eb7fee85193
MJ Rossetti <datacreativellc@gmail.com>
2018-09-21 20:31:25 -0400

    Get professional

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'rspec'
 require 'stripe'
 require 'stripe_mock'
 require 'stripe_mock/server'
-require 'pry' # can I haz debug?
+require 'pry'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
