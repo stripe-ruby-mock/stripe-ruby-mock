b0d024261fb35d8ec6a07e55ad6479713a25cc2c
rd <rd@fractalgarden.com>
2019-09-30 11:10:01 +0200

    fix merge

--- lib/stripe_mock/data.rb
+++ lib/stripe_mock/data.rb
@@ -1150,7 +1150,7 @@ module StripeMock
           client_secret: "pi_1EwXFB2eZvKYlo2CggNnFBo8_secret_vOMkpqZu8ca7hxhfiO80tpT3v",
           confirmation_method: "manual",
           created: 1563208901,
-          currency: "gbp",
+          currency: currency,
           customer: nil,
           description: nil,
           invoice: nil,
@@ -1197,48 +1197,7 @@ module StripeMock
           metadata: {
             "order_id": "123456789"
           }
-        id: payment_intent_id,
-        object: "payment_intent",
-        amount: 49900,
-        amount_capturable: 0,
-        amount_received: 0,
-        application: nil,
-        application_fee_amount: nil,
-        canceled_at: nil,
-        cancellation_reason: nil,
-        capture_method: "automatic",
-        charges: {
-          object: "list",
-          data: [],
-          has_more: false,
-          total_count: 0,
-          url: "/v1/charges?payment_intent=pi_1EwXFB2eZvKYlo2CggNnFBo8"
-        },
-        client_secret: "pi_1EwXFB2eZvKYlo2CggNnFBo8_secret_vOMkpqZu8ca7hxhfiO80tpT3v",
-        confirmation_method: "manual",
-        created: 1563208901,
-        currency: currency,
-        customer: nil,
-        description: nil,
-        invoice: nil,
-        last_payment_error: nil,
-        livemode: false,
-        metadata: {},
-        next_action: nil,
-        on_behalf_of: nil,
-        payment_method: nil,
-        payment_method_types: [
-          "card"
-        ],
-        receipt_email: nil,
-        review: nil,
-        setup_future_usage: nil,
-        shipping: nil,
-        source: nil,
-        statement_descriptor: nil,
-        status: "requires_action",
-        transfer_data: nil,
-        transfer_group: nil
+
       }.merge(params)
     end
     
