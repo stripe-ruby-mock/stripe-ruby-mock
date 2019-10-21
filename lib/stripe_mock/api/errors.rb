65b45e3714f5c5ee2ca3f9889d16076646a6368f
Klaus Zanders <klaus.zanders@ioki.com>
2019-10-21 18:14:43 +0200

    Refactor CardErrors module

--- lib/stripe_mock/api/errors.rb
+++ lib/stripe_mock/api/errors.rb
@@ -15,28 +15,27 @@ module StripeMock
   def self.prepare_card_error(code, *handler_names)
     handler_names.push(:new_charge) if handler_names.count == 0
 
-    error = CardErrors.argument_map[code]
+    error = CardErrors.build_error_for(code)
     raise StripeMockError.new("Unrecognized stripe card error code: #{code}") if error.nil?
     self.prepare_error error, *handler_names
   end
 
   module CardErrors
-
-    def self.argument_map
-      @__map ||= {
-        incorrect_number: add_json_body("The card number is incorrect", 'number', code: 'incorrect_number', http_status: 402),
-        invalid_number: add_json_body("The card number is not a valid credit card number", 'number', code:  'invalid_number', http_status: 402),
-        invalid_expiry_month: add_json_body("The card's expiration month is invalid", 'exp_month', code:  'invalid_expiry_month', http_status: 402),
-        invalid_expiry_year: add_json_body("The card's expiration year is invalid", 'exp_year', code:  'invalid_expiry_year', http_status: 402),
-        invalid_cvc: add_json_body("The card's security code is invalid", 'cvc', code:  'invalid_cvc', http_status: 402),
-        expired_card: add_json_body("The card has expired", 'exp_month', code:  'expired_card', http_status: 402),
-        incorrect_cvc: add_json_body("The card's security code is incorrect", 'cvc', code:  'incorrect_cvc', http_status: 402),
-        card_declined: add_json_body("The card was declined", nil, code:  'card_declined', http_status: 402),
-        missing: add_json_body("There is no card on a customer that is being charged.", nil, code:  'missing', http_status: 402),
-        processing_error: add_json_body("An error occurred while processing the card", nil, code:  'processing_error', http_status: 402),
-        card_error: add_json_body('The card number is not a valid credit card number.', 'number', code:  'invalid_number', http_status: 402),
-        incorrect_zip: add_json_body('The zip code you supplied failed validation.', 'address_zip', code:  'incorrect_zip', http_status: 402)
-      }
+    def self.build_error_for(code)
+      case code
+      when :incorrect_number then build_card_error("The card number is incorrect", 'number', code: 'incorrect_number', http_status: 402)
+      when :invalid_number then build_card_error("The card number is not a valid credit card number", 'number', code:  'invalid_number', http_status: 402)
+      when :invalid_expiry_month then build_card_error("The card's expiration month is invalid", 'exp_month', code:  'invalid_expiry_month', http_status: 402)
+      when :invalid_expiry_year then build_card_error("The card's expiration year is invalid", 'exp_year', code:  'invalid_expiry_year', http_status: 402)
+      when :invalid_cvc then build_card_error("The card's security code is invalid", 'cvc', code:  'invalid_cvc', http_status: 402)
+      when :expired_card then build_card_error("The card has expired", 'exp_month', code:  'expired_card', http_status: 402)
+      when :incorrect_cvc then build_card_error("The card's security code is incorrect", 'cvc', code:  'incorrect_cvc', http_status: 402)
+      when :card_declined then build_card_error("The card was declined", nil, code:  'card_declined', http_status: 402)
+      when :missing then build_card_error("There is no card on a customer that is being charged.", nil, code:  'missing', http_status: 402)
+      when :processing_error then build_card_error("An error occurred while processing the card", nil, code:  'processing_error', http_status: 402)
+      when :card_error then build_card_error('The card number is not a valid credit card number.', 'number', code:  'invalid_number', http_status: 402)
+      when :incorrect_zip then build_card_error('The zip code you supplied failed validation.', 'address_zip', code:  'incorrect_zip', http_status: 402)
+      end
     end
 
     def self.get_decline_code(code)
@@ -50,7 +49,7 @@ module StripeMock
       decline_code_map[code_key]
     end
 
-    def self.add_json_body(message, param, **kwargs)
+    def self.build_card_error(message, param, **kwargs)
       json_hash = {
         message: message,
         param: param,
@@ -59,7 +58,7 @@ module StripeMock
         decline_code: get_decline_code(kwargs[:code])
       }
 
-      error_keyword_args = kwargs.merge({ json_body: { error: json_hash }, http_body: { error: json_hash }.to_json })
+      error_keyword_args = kwargs.merge(json_body: { error: json_hash }, http_body: { error: json_hash }.to_json)
 
       Stripe::CardError.new(message, param, **error_keyword_args)
     end
