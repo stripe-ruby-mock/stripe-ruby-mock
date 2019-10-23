72021c85b1a9fb5468368e8bd90074c9cab47801
Klaus Zanders <klaus.zanders@ioki.com>
2019-10-23 11:11:43 +0200

    Fix usage of double quotes and unneeded self

--- lib/stripe_mock/api/errors.rb
+++ lib/stripe_mock/api/errors.rb
@@ -1,5 +1,4 @@
 module StripeMock
-
   def self.prepare_error(stripe_error, *handler_names)
     handler_names.push(:all) if handler_names.count == 0
 
@@ -16,25 +15,28 @@ module StripeMock
     handler_names.push(:new_charge) if handler_names.count == 0
 
     error = CardErrors.build_error_for(code)
-    raise StripeMockError.new("Unrecognized stripe card error code: #{code}") if error.nil?
-    self.prepare_error error, *handler_names
+    if error.nil?
+      raise StripeMockError, "Unrecognized stripe card error code: #{code}"
+    end
+
+    prepare_error error, *handler_names
   end
 
   module CardErrors
     def self.build_error_for(code)
       case code
-      when :incorrect_number then build_card_error("The card number is incorrect", 'number', code: 'incorrect_number', http_status: 402)
-      when :invalid_number then build_card_error("The card number is not a valid credit card number", 'number', code:  'invalid_number', http_status: 402)
-      when :invalid_expiry_month then build_card_error("The card's expiration month is invalid", 'exp_month', code:  'invalid_expiry_month', http_status: 402)
-      when :invalid_expiry_year then build_card_error("The card's expiration year is invalid", 'exp_year', code:  'invalid_expiry_year', http_status: 402)
-      when :invalid_cvc then build_card_error("The card's security code is invalid", 'cvc', code:  'invalid_cvc', http_status: 402)
-      when :expired_card then build_card_error("The card has expired", 'exp_month', code:  'expired_card', http_status: 402)
-      when :incorrect_cvc then build_card_error("The card's security code is incorrect", 'cvc', code:  'incorrect_cvc', http_status: 402)
-      when :card_declined then build_card_error("The card was declined", nil, code:  'card_declined', http_status: 402)
-      when :missing then build_card_error("There is no card on a customer that is being charged.", nil, code:  'missing', http_status: 402)
-      when :processing_error then build_card_error("An error occurred while processing the card", nil, code:  'processing_error', http_status: 402)
-      when :card_error then build_card_error('The card number is not a valid credit card number.', 'number', code:  'invalid_number', http_status: 402)
-      when :incorrect_zip then build_card_error('The zip code you supplied failed validation.', 'address_zip', code:  'incorrect_zip', http_status: 402)
+      when :incorrect_number then build_card_error('The card number is incorrect', 'number', code: 'incorrect_number', http_status: 402)
+      when :invalid_number then build_card_error('The card number is not a valid credit card number', 'number', code:  'invalid_number', http_status: 402)
+      when :invalid_expiry_month then build_card_error("The card's expiration month is invalid", 'exp_month', code: 'invalid_expiry_month', http_status: 402)
+      when :invalid_expiry_year then build_card_error("The card's expiration year is invalid", 'exp_year', code: 'invalid_expiry_year', http_status: 402)
+      when :invalid_cvc then build_card_error("The card's security code is invalid", 'cvc', code: 'invalid_cvc', http_status: 402)
+      when :expired_card then build_card_error('The card has expired', 'exp_month', code: 'expired_card', http_status: 402)
+      when :incorrect_cvc then build_card_error("The card's security code is incorrect", 'cvc', code: 'incorrect_cvc', http_status: 402)
+      when :card_declined then build_card_error('The card was declined', nil, code: 'card_declined', http_status: 402)
+      when :missing then build_card_error('There is no card on a customer that is being charged.', nil, code: 'missing', http_status: 402)
+      when :processing_error then build_card_error('An error occurred while processing the card', nil, code: 'processing_error', http_status: 402)
+      when :card_error then build_card_error('The card number is not a valid credit card number.', 'number', code: 'invalid_number', http_status: 402)
+      when :incorrect_zip then build_card_error('The zip code you supplied failed validation.', 'address_zip', code: 'incorrect_zip', http_status: 402)
       end
     end
 
