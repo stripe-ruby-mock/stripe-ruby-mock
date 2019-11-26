41f7f741ba11c6707c733915038388b72188a9cd
Seth Krasnianski <sethkrasnianski@gmail.com>
2019-11-26 10:41:43 -0500

    Prevent customer metadata from being overwritten with each update

--- spec/shared_stripe_examples/customer_examples.rb
+++ spec/shared_stripe_examples/customer_examples.rb
@@ -372,6 +372,15 @@ shared_examples 'Customer API' do
     expect(customer.discount.coupon).to be_a Stripe::Coupon
   end
 
+  it "preserves stripe customer metadata" do
+    metadata = {user_id: "38"}
+    customer = Stripe::Customer.create(metadata: metadata)
+    expect(customer.metadata.to_h).to eq(metadata)
+
+    updated = Stripe::Customer.update(customer.id, metadata: {fruit: "apples"})
+    expect(updated.metadata.to_h).to eq(metadata.merge(fruit: "apples"))
+  end
+
   it "retrieves the customer's default source after it was updated" do
     customer = Stripe::Customer.create()
     customer.source = gen_card_tk
