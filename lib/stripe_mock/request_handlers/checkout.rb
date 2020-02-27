7c1962d002888cdf0f9eeaba9e47c6b62b01ac14
Shunichi Ikegami <sike.tm@gmail.com>
2020-02-27 13:33:08 +0900

    Basic idempotency_key functionality for checkout session

--- lib/stripe_mock/request_handlers/checkout.rb
+++ lib/stripe_mock/request_handlers/checkout.rb
@@ -6,6 +6,14 @@ module StripeMock
       end
 
       def new_session(route, method_url, params, headers)
+        if headers && headers[:idempotency_key]
+          params[:idempotency_key] = headers[:idempotency_key]
+          if checkout_sessions.any?
+            original_checkout_session = checkout_sessions.values.find { |s| s[:idempotency_key] == headers[:idempotency_key]}
+            return original_checkout_session if original_checkout_session
+          end
+        end
+
         params[:id] ||= new_id('cs')
 
         checkout_sessions[params[:id]] = Data.mock_checkout_session(params)
