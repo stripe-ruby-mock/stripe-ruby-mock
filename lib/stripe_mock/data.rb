0f0d2ac2db81147f442fd8e5b0ccd8d606b1727b
Fabiano Arruda <fabianoarruda@gmail.com>
2023-08-10 20:48:27 -0300

    Adds missing attributes to subscriptions and Invoice line items

--- lib/stripe_mock/data.rb
+++ lib/stripe_mock/data.rb
@@ -492,6 +492,7 @@ module StripeMock
         amount: 1000,
         currency: currency,
         discountable: false,
+        discount_amounts: [],
         proration: false,
         period: {
           start: 1349738920,
